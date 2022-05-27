2baa1d33e98c73e052a055bdb1ec263782b73cc4
rosannagamal <101247652+rosannagamal@users.noreply.github.com>
2022-05-28 00:37:35 +0200

    Add files via upload

---
+++ library-management-gui/HomePage/bookinfo_page.py
@@ -0,0 +1,91 @@
+import os
+from tkinter import *
+from PIL import ImageTk, Image
+from library.library import Library
+
+# Instance of library class
+library = Library()
+
+
+class FoundBook(Toplevel):
+    def __init__(self, book, user):
+        Toplevel.__init__(self)
+
+        self.book = book
+        self.user = user
+
+        # Frame 4
+        self.title('Search')
+        self.geometry('900x500')
+        self.config(bg='#07272C')
+
+        self.frame()    # Frame Picture
+
+        self.book_cover()   # Displays Book cover
+
+        # Book Info Labels
+        self.book_title()
+        self.book_author()
+        self.book_isbn()
+        self.book_type()
+
+        # Borrow Book Button
+        self.button()
+
+    def frame(self):
+        # Frame Picture
+        img = ImageTk.PhotoImage(file='/Users/rosannagamal/assignment_two/HomePage/bookpage.png')
+        Label(self, image=img, bg='#061A1D').place(x=0, y=0)
+
+    def book_cover(self):
+        # Cover Picture
+        file = os.path.join('/Users/rosannagamal/assignment_two/book_covers', self.book.file)
+        img = ImageTk.PhotoImage(file=file)
+        Label(self, image=img, bg='#061A1D').place(x=58, y=68)
+
+    def book_title(self):
+        # Title Label
+        title_label = Label(self, text='Title', fg='white', bg='#08262A', font=('Arial', 15, 'bold'))
+        title_label.place(x=392, y=99)
+        # Book Title
+        title = Label(self, text=self.book.title, fg='white', bg='#08262A', font=('Arial', 15))
+        title.place(x=524, y=99)
+
+    def book_author(self):
+        # Author Label
+        author_label = Label(self, text='Author', fg='white', bg='#08262A', font=('Arial', 15, 'bold'))
+        author_label.place(x=392, y=160)
+        # Book Author
+        author = Label(self, text=self.book.author, fg='white', bg='#08262A', font=('Arial', 15 ))
+        author.place(x=524, y=160)
+
+    def book_isbn(self):
+        # ISBN Label
+        isbn_label = Label(self, text='ISBN', fg='white', bg='#08262A', font=('Arial', 15, 'bold'))
+        isbn_label.place(x=392, y=222)
+        # Book ISBN
+        isbn = Label(self, text=self.book.isbn, fg='white', bg='#08262A', font=('Arial', 15))
+        isbn.place(x=524, y=222)
+
+    def book_type(self):
+        # Book Type Label Label
+        book_type_label = Label(self, text='Type',  fg='white', bg='#08262A', font=('Arial', 15, 'bold'))
+        book_type_label.place(x=392, y=282)
+        # Converting number to appropriate text
+        book_type = 'Hard Copy' if self.book.online_physical == '1' else 'Soft Copy'
+        # Book Type
+        online_physical = Label(self, text=book_type,  fg='white', bg='#08262A', font=('Arial', 15))
+        online_physical.place(x=637, y=282)
+
+    def borrow_book(self):
+        # Borrow book function from library class
+        user = library.find_user(library.registered, self.user)
+        library.borrow_book(library.books, user, self.book.title)
+        Label(self, text='Book Borrowed.', bg='#061A1D', font=('Inter', 15, 'bold'), fg='white').place(x=519, y=350)
+
+    def button(self):
+        # Creating Label for Button
+        button_label = Label(self, text='Borrow', font=('Inter', 15, 'bold'), fg='white', bg='#08262A', cursor='hand')
+        button_label.place(x=409, y=350)
+        user = library.find_user(library.registered, self.user)
+        button_label.bind('<Button-1>',lambda event: self.borrow_book())
---
+++ library-management-gui/README.md
@@ -0,0 +1,77 @@
+# library-management-gui
+GUI for the Library Management System (A1, A2)
+
+### Table of Contents  
+[Functionalities](#headers) 
+
+[List of Registered Users](#headers) 
+
+[List of Books](#headers) 
+
+[Screenshots](#headers) 
+
+
+<a name="headers"/>
+
+### Library Functionalities:
+
+* SignIn
+* SignUp
+* Search For Book 
+* Page Displaying Data Dbout Book
+* Borrow Book
+* Data About Most Borrowed Book
+* Books Borrowed by Student With Date the Book Was Borrowed
+* List of Newest Books
+
+
+### List of Already Registered Users
+* In case you don't feel like creating one.
+
+Username    | Password
+----------- | ------------------
+rosannagamal| rosanna123
+milo      	| milo123
+saragamal   | sara123
+
+
+</br>
+
+### Books You Can Borrow
+
+Title        	   
+---------------------------------|         
+1984	 	   
+Pride and Prejudice	 
+The Midnight Library		     
+The Invisible Life of Addie LaRue
+Where the Crawdads Sing
+The Seven Husbands of Evelyn Hugo
+The Great Gatsby
+
+</br>
+
+### SignIn Page 
+
+<img src="https://user-images.githubusercontent.com/101247652/170791203-dd5dfac6-0a78-4d20-9a1f-bbd68875cfed.png" width=50% height=50%>
+</br>
+
+
+### SignUp Page 
+
+<img src="https://user-images.githubusercontent.com/101247652/170791358-f6a528ce-4af4-47e5-bf66-6138f71813a2.png" width=50% height=50%>
+</br>
+
+
+### Home Page 
+
+<img src="https://user-images.githubusercontent.com/101247652/170791541-3d7efa4f-617e-494d-a84c-8db42dddb766.png" width=50% height=50%>
+</br>
+
+
+### Search Function Result Page 
+
+<img src="https://user-images.githubusercontent.com/101247652/170791447-05ec8685-7aa8-4567-a18c-03572df95872.png" width=50% height=50%>
+</br>
+
+
